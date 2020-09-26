f20f342702ef454227881822992969b4ede050e1
raju-ahammad <web.rajuahammad@gmail.com>
2020-09-27 01:58:34 +0600

    create login

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
+function Welcome() {
+  return <h1>WelCome Back!</h1>
+}
+
+function Please() {
+  return <h1>Please SignUp</h1>
+}
+
+function User(props) {
+  const isLogin=props.isLogin;
+  if(isLogin) {
+    return <Welcome/>
+  }
+  return <Please/>
+
+}
+
+
+const LogInBtn = ({ handleLogIn })=> {
+  return <button className="bg-success py-1 px-3" onClick={handleLogIn}>Login</button>
+}
+const LogOutBtn = ({ handleLogOut })=> {
+  return <button className="bg-danger py-1 px-3" onClick={handleLogOut}>Logout</button>
+}
 
 class App extends Component {
   state = {
-    isToggle: true
+    isToggle: true,
+    isLogin: false
   }
   handleClick = ()=> {
     this.setState({
@@ -10,10 +36,32 @@ class App extends Component {
     })
   }
 
+  handleLogIn = () => {
+    this.setState({
+      isLogin: !this.state.isLogin
+    })
+  }
+  handleLogOut = () => {
+    this.setState({
+      isLogin: !this.state.isLogin
+    })
+  }
+
   render() {
+    const isLogin = this.state.isLogin
+    let button;
+    if (isLogin) {
+      button = <LogOutBtn handleLogOut={this.handleLogOut}/>
+    } else {
+      button = <LogInBtn handleLogIn={this.handleLogIn}/>
+    }
     return (
       <div className={this.state.isToggle ? "bg-dark": "container mt-5"}>
         <button className={this.state.isToggle ? "bg-primary text-dark":"bg-dark text-light"} onClick={this.handleClick}>{this.state.isToggle ? "ON":"OFF"}</button>
+        <div className="m-4">
+          <User isLogin= {isLogin}/>
+          { button }
+        </div>
       </div>
     )
   }
